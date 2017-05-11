cb0c419dba1f6f25347993f2f3d0926ca1b672cd
HNMN3 <hnmn3.nitin@gmail.com>
2017-05-11 11:59:41 +0530

    serializer created

--- SearchUser/models.py
+++ SearchUser/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class GithubUser(models.Model):
+class GitHubUser(models.Model):
     login = models.CharField(max_length=50)
     id = models.IntegerField()
     avatar_url = models.TextField()
---
+++ SearchUser/serializers.py
@@ -0,0 +1,10 @@
+# Keep coding and change the world..And do not forget anything..Not Again..
+from rest_framework import serializers
+from .models import GitHubUser
+
+
+class GitHubUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GitHubUser
+        fields = '__all__'
+
